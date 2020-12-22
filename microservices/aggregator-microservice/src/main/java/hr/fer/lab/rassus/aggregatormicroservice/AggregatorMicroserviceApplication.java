ae9927a8f1a5b5217ee5d33fcee192789af86256
Andrej Gregic <andgre@enea.se>
2020-12-22 15:03:00 +0100

    popravljen config server

--- microservices/aggregator-microservice/src/main/java/hr/fer/lab/rassus/aggregatormicroservice/AggregatorMicroserviceApplication.java
+++ microservices/aggregator-microservice/src/main/java/hr/fer/lab/rassus/aggregatormicroservice/AggregatorMicroserviceApplication.java
@@ -1,14 +1,19 @@
 package hr.fer.lab.rassus.aggregatormicroservice;
 
+import java.net.ConnectException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import com.netflix.discovery.shared.transport.TransportException;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AggregatorMicroserviceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ConnectException {
+		System.setProperty("spring.cloud.bootstrap.enabled","true");
 		SpringApplication.run(AggregatorMicroserviceApplication.class, args);
 	}
 
