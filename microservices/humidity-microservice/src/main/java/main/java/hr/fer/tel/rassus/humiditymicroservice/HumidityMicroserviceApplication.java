0923437f4d9cb0e6668a55ab4babb5e30fafa476
petra <pk119123344@fer.hr>
2021-01-03 11:41:20 +0100

    dodano ucitavanje iz datoteke mjerenje.csv
    nesto ne radi sa serverom

--- microservices/humidity-microservice/src/main/java/main/java/hr/fer/tel/rassus/humiditymicroservice/HumidityMicroserviceApplication.java
+++ microservices/humidity-microservice/src/main/java/main/java/hr/fer/tel/rassus/humiditymicroservice/HumidityMicroserviceApplication.java
@@ -9,6 +9,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.io.BufferedReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class HumidityMicroserviceApplication implements CommandLineRunner {
@@ -23,14 +29,28 @@ public class HumidityMicroserviceApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         if (humidityDataRepository.count() == 0){
-            
-            HumidityData h1 = new HumidityData();
+            Path pathToFile = Paths.get("src/main/resources/mjerenja.csv");
+            try (BufferedReader br = Files.newBufferedReader(pathToFile.toAbsolutePath(),
+                    StandardCharsets.US_ASCII)) {
+                String line = br.readLine();
+                while ( line != null){
+                    line = br.readLine();
+                    String[] attributes = line.split(",");
+                    HumidityData h = new HumidityData();
+                    if( attributes[2] == null)
+                        h.setPercentageOfHumidy("0");
+                    else
+                        h.setPercentageOfHumidy(attributes[2]);
+                    humidityDataRepository.save(h);
+                }
+            }
+            /*HumidityData h1 = new HumidityData();
             h1.setPercentageOfHumidy("1254");
             //TODO ovdje je potrebno promjeniti ucitavanje iz csv datoteke i da se to spremi
             humidityDataRepository.save(h1);
             HumidityData h2 = new HumidityData();
-            h1.setPercentageOfHumidy("5689");
-            humidityDataRepository.save(h2);
+            h2.setPercentageOfHumidy("5689");
+            humidityDataRepository.save(h2);*/
 
         }
     }
